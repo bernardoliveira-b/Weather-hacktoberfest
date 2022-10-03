b1f897bc9390c7c9e4a8952a6b439b74acdef8da
Lucas Siqueira Fernandes <lucas.fernandes@shift.com>
2022-10-03 01:36:12 -0300

    Setting dynamic project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,31 @@
 class HomeController < ApplicationController
+  before_action :load_states
   before_action :weather_response
 
   def index
   end
 
+  def consult
+    Rails.cache.fetch("districts_#{params[:state]}", expires_in: 12.hours) do
+      CityService.new(params[:state]).call
+    end
+
+    if params[:city].present?
+      Rails.cache.fetch("city_#{params[:city]}", expires_in: 12.hours) do
+        WeatherService.new(params[:city]).call
+      end
+    end
+
+    redirect_to root_path(state: params[:state], city: params[:city])
+  end
+
   private
 
+  def load_states
+    @states = StateService.new.call
+  end
+
   def weather_response
-    @weather_response = WeatherService.new.call
+    @weather_response = Rails.cache.read("city_#{params[:city]}")
   end
 end
